5b1f474645948b4b5985497bc82e0c7455ea6a31
Tharaka Basuru <it20391768@my.sliit.lk>
2023-05-14 14:38:27 +0530

    push

---
+++ server/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "MH+yGZppyGrHWgen3CERGu/Y+9Mpt/MSWbHPhMt1TCE=",
+    EMAIL :"melyna.crona@ethereal.email",
+    PASSWARD : "fpwBgCnUj6zAeZD5q2",
+    ATLAS_URI : "mongodb+srv://admin:admin@cluster0.xidzuob.mongodb.net/?retryWrites=true&w=majority"
+}
